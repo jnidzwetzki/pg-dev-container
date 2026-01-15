6df745b29006c73fceaede1c08328cb6425fba9e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2026-01-15 21:54:33 +0000

    Bump to 18.1

--- src/extensions/06_scan/scan.c
+++ src/extensions/06_scan/scan.c
@@ -35,7 +35,7 @@ static void print_tuple(TupleTableSlot *slot, TupleDesc tupdesc)
             Oid typoid;
 
             /* lookup output func for the type */
-            typoid = tupdesc->attrs[i].atttypid;
+            typoid = TupleDescAttr(tupdesc, i)->atttypid;
             getTypeOutputInfo(typoid, &foutoid, &typisvarlena);
             text_value = OidOutputFunctionCall(foutoid, value);
         }
@@ -54,6 +54,7 @@ Datum full_table_scan(PG_FUNCTION_ARGS)
     TableScanDesc tablescandesc;
     TupleTableSlot *slot;
     TupleDesc tupdesc;
+    Snapshot snapshot;
     Oid relid = PG_ARGISNULL(0) ? InvalidOid : PG_GETARG_OID(0);
 
     if (relid == InvalidOid)
@@ -68,12 +69,13 @@ Datum full_table_scan(PG_FUNCTION_ARGS)
     tupdesc = slot->tts_tupleDescriptor;
 
     /* Perform a full table scan */
-    tablescandesc = table_beginscan(relation, GetTransactionSnapshot(), 0, NULL);
+    snapshot = RegisterSnapshot(GetTransactionSnapshot());
+    tablescandesc = table_beginscan(relation, snapshot, 0, NULL);
 
     /* Log the attribute names of the table */
     for (int i = 0; i < tupdesc->natts; i++)
     {
-        char *attname = NameStr(relation->rd_att->attrs[i].attname);
+        char *attname = NameStr(TupleDescAttr(RelationGetDescr(relation), i)->attname);
         elog(INFO, "Attribute name %d: %s", i + 1, attname);
     }
     elog(INFO, "==========================");
@@ -90,6 +92,7 @@ Datum full_table_scan(PG_FUNCTION_ARGS)
 
     /* Clean up */
     table_endscan(tablescandesc);
+    UnregisterSnapshot(snapshot);
     ExecDropSingleTupleTableSlot(slot);
     table_close(relation, AccessShareLock);
 
@@ -105,6 +108,7 @@ Datum table_scan_with_scankeys(PG_FUNCTION_ARGS)
     TupleTableSlot *slot;
     TupleDesc tupdesc;
     ScanKeyData scanKeys[2];
+    Snapshot snapshot;
     Oid relid = PG_ARGISNULL(0) ? InvalidOid : PG_GETARG_OID(0);
 
     if (relid == InvalidOid)
@@ -122,21 +126,22 @@ Datum table_scan_with_scankeys(PG_FUNCTION_ARGS)
     if (tupdesc->natts < 2)
         ereport(ERROR, (errmsg("relation has less than 2 attributes")));
 
-    if (tupdesc->attrs[0].atttypid != INT4OID)
+    if (TupleDescAttr(tupdesc, 0)->atttypid != INT4OID)
         ereport(ERROR, (errmsg("first attribute is not of type int4")));
     ScanKeyInit(&scanKeys[0], 1, InvalidStrategy, F_INT4GE, Int64GetDatum(2));
 
-    if (tupdesc->attrs[1].atttypid != TEXTOID)
+    if (TupleDescAttr(tupdesc, 1)->atttypid != TEXTOID)
         ereport(ERROR, (errmsg("second attribute is not of type text")));
     ScanKeyInit(&scanKeys[1], 2, InvalidStrategy, F_TEXTEQ, PointerGetDatum(cstring_to_text("Bob")));
 
     /* Perform a full table scan */
-    tablescandesc = table_beginscan(relation, GetTransactionSnapshot(), 2, scanKeys);
+    snapshot = RegisterSnapshot(GetTransactionSnapshot());
+    tablescandesc = table_beginscan(relation, snapshot, 2, scanKeys);
 
     /* Log the attribute names of the table */
     for (int i = 0; i < tupdesc->natts; i++)
     {
-        char *attname = NameStr(relation->rd_att->attrs[i].attname);
+        char *attname = NameStr(TupleDescAttr(RelationGetDescr(relation), i)->attname);
         elog(INFO, "Attribute name %d: %s", i + 1, attname);
     }
     elog(INFO, "==========================");
@@ -153,6 +158,7 @@ Datum table_scan_with_scankeys(PG_FUNCTION_ARGS)
 
     /* Clean up */
     table_endscan(tablescandesc);
+    UnregisterSnapshot(snapshot);
     ExecDropSingleTupleTableSlot(slot);
     table_close(relation, AccessShareLock);
 
@@ -169,6 +175,7 @@ Datum table_scan_with_index(PG_FUNCTION_ARGS)
     TupleTableSlot *slot;
     TupleDesc tupdesc;
     ScanKeyData scanKeys[2];
+    Snapshot snapshot;
     Oid relid = PG_ARGISNULL(0) ? InvalidOid : PG_GETARG_OID(0);
     Oid indexrelid = PG_ARGISNULL(1) ? InvalidOid : PG_GETARG_OID(1);
 
@@ -193,22 +200,23 @@ Datum table_scan_with_index(PG_FUNCTION_ARGS)
     if (IndexRelationGetNumberOfKeyAttributes(indexrelation) < 2)
         ereport(ERROR, (errmsg("index has less than 2 attributes")));
 
-    if (indexrelation->rd_att->attrs[0].atttypid != INT4OID)
+    if (TupleDescAttr(indexrelation->rd_att, 0)->atttypid != INT4OID)
         ereport(ERROR, (errmsg("first attribute is not of type int4")));
     ScanKeyInit(&scanKeys[0], 1, BTGreaterEqualStrategyNumber, F_INT4GE, Int64GetDatum(2));
 
-    if (indexrelation->rd_att->attrs[1].atttypid != TEXTOID)
+    if (TupleDescAttr(indexrelation->rd_att, 1)->atttypid != TEXTOID)
         ereport(ERROR, (errmsg("second attribute is not of type text")));
     ScanKeyInit(&scanKeys[1], 2, BTEqualStrategyNumber, F_TEXTEQ, PointerGetDatum(cstring_to_text("Bob")));
 
     /* Perform a full index scan */
-    indexscandesc = index_beginscan(relation, indexrelation, GetTransactionSnapshot(), 2, 0);
+    snapshot = RegisterSnapshot(GetTransactionSnapshot());
+    indexscandesc = index_beginscan(relation, indexrelation, snapshot, NULL, 2, 0);
     index_rescan(indexscandesc, scanKeys, 2, NULL, 0);
 
     /* Log the attribute names of the table */
     for (int i = 0; i < tupdesc->natts; i++)
     {
-        char *attname = NameStr(relation->rd_att->attrs[i].attname);
+        char *attname = NameStr(TupleDescAttr(RelationGetDescr(relation), i)->attname);
         elog(INFO, "Attribute name %d: %s", i + 1, attname);
     }
     elog(INFO, "==========================");
@@ -225,6 +233,7 @@ Datum table_scan_with_index(PG_FUNCTION_ARGS)
 
     /* Clean up */
     index_endscan(indexscandesc);
+    UnregisterSnapshot(snapshot);
     ExecDropSingleTupleTableSlot(slot);
     index_close(indexrelation, AccessShareLock);
     table_close(relation, AccessShareLock);
@@ -250,6 +259,7 @@ Datum table_scan_and_sort_attribute(PG_FUNCTION_ARGS)
     Oid attr_type[1];
     Oid attr_collation[1];
     bool sort_nulls_first[1] = {false};
+    Snapshot snapshot;
     Tuplesortstate *tuplesortstate;
 
     if (relid == InvalidOid)
@@ -294,7 +304,8 @@ Datum table_scan_and_sort_attribute(PG_FUNCTION_ARGS)
                                           TUPLESORT_NONE);  /* sortopt */
 
     /* Perform a full table scan */
-    tablescandesc = table_beginscan(relation, GetTransactionSnapshot(), 0, NULL);
+    snapshot = RegisterSnapshot(GetTransactionSnapshot());
+    tablescandesc = table_beginscan(relation, snapshot, 0, NULL);
 
     /* Iterate through the rows of the table */
     while (table_scan_getnextslot(tablescandesc, ForwardScanDirection, slot))
@@ -321,6 +332,7 @@ Datum table_scan_and_sort_attribute(PG_FUNCTION_ARGS)
     /* Clean up */
     tuplesort_end(tuplesortstate);
     table_endscan(tablescandesc);
+    UnregisterSnapshot(snapshot);
     ExecDropSingleTupleTableSlot(sorted_slot);
     table_close(relation, AccessShareLock);
 
